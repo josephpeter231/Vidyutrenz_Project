389f1097c2aa76ff7add479263b176910b4f5e9c
josephpeter231 <josephpeterjcit@gmail.com>
2023-09-02 05:40:47 +0530

    card'

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 //import cd from "../assets/cit.png";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 
 const Card = ({ Event }) => {
   const router = useNavigate();
